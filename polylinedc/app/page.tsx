37fa890f8a63322a4afe7f3a4cb5a55a4294c35d
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-08-30 00:03:28 +0300

    serxere nu

---
+++ polylinedc/app/page.tsx
@@ -0,0 +1,68 @@
+// Bismillahirahmanirahim
+
+"use client"
+
+import Image from 'next/image'
+
+import dudu from '/public/m.jpeg'
+
+
+import { Container, Row,Col, Alert } from 'react-bootstrap'
+import KitchenSinkExample from './components/card'
+
+import mmdudu from '/public/mm1.jpg'
+
+import mmsise from '/public/mm.jpg'
+
+import mmm from '/public/f1.jpg'
+
+
+
+
+
+
+
+
+
+
+
+
+function page() {
+  return (
+    <div>
+
+     
+<Container>
+<Alert style={{justifyContent:'center',background:'white',margin:'7px'}}>
+<h1>Yapım Aşamasında</h1>  
+
+
+
+</Alert>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</Container>
+
+
+    </div>
+  )
+}
+
+export default page;
+
+
