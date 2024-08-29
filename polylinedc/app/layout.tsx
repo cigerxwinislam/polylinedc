ab705c814e581250ad3689dd2b7b31592dac8c04
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-08-30 01:28:16 +0300

    serxere

--- polylinedc/app/layout.tsx
+++ polylinedc/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{background:"green",color:"white"}} className={inter.className}>
+      <body style={{background:"black",color:"white"}} className={inter.className}>
         
         <BasicExample/>
         {children}
