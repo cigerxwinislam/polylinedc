b5cf5a560306bba8418111ac210f68f61ec76ea4
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-09-14 21:25:17 +0300

    Update tegihistin.jsx

--- polylinedc/app/components/tegihistin.jsx
+++ polylinedc/app/components/tegihistin.jsx
@@ -10,10 +10,10 @@ import { WhatsAppWidget } from 'react-whatsapp-widget';
 export function Tegihistin() {
   return (<div>
     <Alert style={{margin:7 ,background:"white"}} >
-     <Button  href="tel:+905549765692"><FaPhone  /></Button> Telefon Et
+     <Button  href="tel:+7 999 397-60-38"><FaPhone  /></Button> Telefon Et
 
 
-  <WhatsAppWidget message=" Whatsapptan Mesajınızı Yazın" inputPlaceHolder="Mesajınızı Yazın" replyTimeText="En kısa zamanda dönüş yapılacaktır" sendButtonText="Gönder" companyName="" phoneNumber="+905549765692" ></WhatsAppWidget>
+  <WhatsAppWidget message=" Whatsapptan Mesajınızı Yazın" inputPlaceHolder="Mesajınızı Yazın" replyTimeText="En kısa zamanda dönüş yapılacaktır" sendButtonText="Gönder" companyName="" phoneNumber="+7 999 397-60-38 "></WhatsAppWidget>
 
   </Alert>
     </div>
