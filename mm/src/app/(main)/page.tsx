3daffefbd425f80954019d66a17196607e69ad83
cigerxwinislam <islamtunc748@gmail.com>
2024-12-28 01:08:30 +0300

    serxere nu

--- mm/src/app/(main)/page.tsx
+++ mm/src/app/(main)/page.tsx
@@ -1,28 +1,13 @@
-// Bismillahirahmanirahim
+// Bismillahirrahmanirrahim
 
 
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import FollowingFeed from "./FollowingFeed";
-import ForYouFeed from "./ForYouFeed";
+import React from 'react'
 
-export default function Home() {
+function page() {
   return (
-    <main className="flex w-full min-w-0 gap-5">
-      <div className="w-full min-w-0 space-y-5">
-        <Tabs defaultValue="for-you">
-          <TabsList>
-            <TabsTrigger value="for-you">For you</TabsTrigger>
-            <TabsTrigger value="following">Following</TabsTrigger>
-          </TabsList>
-          <TabsContent value="for-you">
-            <ForYouFeed />
-          </TabsContent>
-          <TabsContent value="following">
-            <FollowingFeed />
-          </TabsContent>
-        </Tabs>
-      </div>
-    </main>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
