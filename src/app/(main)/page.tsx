e8155a9fd16b3db0ca1044b78c528d21b44f748b
cigerxwinislam <islamtunc748@gmail.com>
2024-12-27 23:13:48 +0300

    SERXERE NU

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,8 +2,6 @@
 
 
 
-import PostEditor from "@/components/posts/editor/PostEditor";
-import TrendsSidebar from "@/components/TrendsSidebar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import FollowingFeed from "./FollowingFeed";
 import ForYouFeed from "./ForYouFeed";
@@ -12,7 +10,6 @@ export default function Home() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
-        <PostEditor />
         <Tabs defaultValue="for-you">
           <TabsList>
             <TabsTrigger value="for-you">For you</TabsTrigger>
@@ -26,7 +23,6 @@ export default function Home() {
           </TabsContent>
         </Tabs>
       </div>
-      <TrendsSidebar />
     </main>
   );
 }
