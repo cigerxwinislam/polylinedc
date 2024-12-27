65e1bbc0e9ec8cd8d09a3a367a7d99637445ba19
cigerxwinislam <islamtunc748@gmail.com>
2024-12-28 01:26:12 +0300

    serxere nu

--- mm/src/app/(main)/page.tsx
+++ mm/src/app/(main)/page.tsx
@@ -1,13 +1,48 @@
 // Bismillahirrahmanirrahim
 
+// Elhamdullilahirabbul'alemin
 
+import React, { useEffect } from 'react';
 
-import React from 'react'
-
-function page() {
-  return (
-    <div>page</div>
-  )
+declare global {
+  interface Window {
+    onYouTubeIframeAPIReady: () => void;
+    YT: any;
+  }
 }
 
-export default page
+function page() {
+  useEffect(() => {
+    const tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    if (firstScriptTag.parentNode) {
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+
+    window.onYouTubeIframeAPIReady = function() {
+      new window.YT.Player('player', {
+        videoId: 'nGnrGoR2bt4', // Replace with your YouTube video ID
+        events: {
+          'onReady': (event: { target: { playVideo: () => void; mute: () => void; }; }) => {
+            event.target.playVideo();
+            event.target.mute(); // Mute the video if you only want the audio
+          }
+        },
+        playerVars: {
+          'autoplay': 1,
+          'loop': 1,
+          'playlist': 'YOUR_VIDEO_ID' // Required for looping
+        }
+      });
+    };
+  }, []);
+
+  return (
+    <div>
+      <div id="player"></div>
+    </div>
+  );
+}
+
+export default page;
